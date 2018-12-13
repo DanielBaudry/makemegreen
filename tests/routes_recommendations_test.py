fac3756f65d4fecf98d754cb7fa103cd04402c41
Ben <b.vanderlynden@gmail.com>
2018-12-13 19:32:53 +0100

    recup test

--- tests/routes_recommendations_test.py
+++ tests/routes_recommendations_test.py
@@ -6,18 +6,17 @@ import pytest
 from models import BaseObject
 from models.user import User
 from tests.conftest import clean_database
-from utils.test_utils import API_URL, req, req_with_auth, create_user
+from utils.test_utils import API_URL, req, create_user
 
 BASE_DATA = {
     'email': 'test@test.fr',
     'password': 'test12345678',
 }
 
+
 #ToDo : à mettre dans route_test-reco.py
 @pytest.mark.standalone
 def test_get_search_recommandation_in_title():
-    data = BASE_DATA.copy()
-    data['password'] = 'short'
-
-    r = req.get(API_URL + '/recommandations/search?search="blablablablabla"')
+    r = req.get(API_URL + '/recommendations/search?search="blablablablabla"')
     assert r.status_code == 200
+
--- utils/test_utils.py
+++ utils/test_utils.py
@@ -13,24 +13,6 @@ from utils.token import random_token
 
 API_URL = "http://localhost:5000"
 
-def req_with_auth(email=None, password=None):
-    request = req.Session()
-    if email is None:
-        request.auth = ('pctest.admin@btmx.fr', 'pctestadmin')
-    elif password is not None:
-        request.auth = (email, password)
-    else:
-        json_path = Path(path.dirname(path.realpath(__file__))) / '..' / 'mock' / 'jsons' / 'users.json'd
-
-        with open(json_path) as json_file:
-            for user_json in json.load(json_file):
-                print('user_json', user_json)
-                if email == user_json['email']:
-                    request.auth = (user_json['email'], user_json['password'])
-                    break
-                raise ValueError("Utilisateur inconnu: " + email)
-    return request
-
 
 def create_user(email='john.doe@test.com', 
                 password='totallysafepsswd'):
