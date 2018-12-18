f63c81641facfbd7b2933777f19406a7a849d485
Ben <b.vanderlynden@gmail.com>
2018-12-18 11:46:37 +0100

    corrections after change request

--- tests/routes_recommendations_test.py
+++ tests/routes_recommendations_test.py
@@ -4,19 +4,19 @@ from unittest.mock import patch
 import pytest
 
 from models import BaseObject
-from models.user import User
+from models.recommendation import Recommendation
 from tests.conftest import clean_database
-from utils.test_utils import API_URL, req, create_user
+#TODO : from utils.test_utils import API_URL, req, create_recommendation
 
 BASE_DATA = {
-    'email': 'test@test.fr',
-    'password': 'test12345678',
+    'title': 'Nouvelle recommandation pour faire un tests',
+    'content': 'Contenu reco 1',
 }
-
 
 #ToDo : à mettre dans route_test-reco.py
 @pytest.mark.standalone
 def test_get_search_recommandation_in_title():
+
     r = req.get(API_URL + '/recommendations/search?search="blablablablabla"')
     assert r.status_code == 200
 
