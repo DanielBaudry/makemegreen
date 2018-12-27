40729846cc447167e6225de481c1d03e0554427c
DanielBaudry <daniel.baudry2@gmail.com>
2018-12-27 15:13:46 +0100

    [WEBAPP] design proposition page

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ insert into footprint VALUES (3, 1,'2018-09-29 16:06:42.434928', 'waste', 120, n
 insert into recommendation VALUES (1, 4, 'Vive le vélo','Un vélo ça polue vraiment moins quand même !',20,2,60,'carbon','2018-09-29 16:06:42.434928');
 insert into recommendation VALUES (2, 4, 'Prends une douche','Arrêtes les bains et prends des douches !',3,1,120,'water','2018-09-29 16:06:42.434928');
 insert into recommendation VALUES (3, 4, 'Achète du verre non teinté','Le seul verre recyclable !',7,2,30,'waste','2018-09-29 16:06:42.434928');
-insert into recommendation VALUES (4, 4, 'Eponge durable','Acheter une éponge durable c''est cool !',2,1,40,'waste','2018-10-27 18:37:42.434928');
-insert into recommendation VALUES (5, 4, 'Brosse a dent durable','Acheter une brosse a dent durable c''est cool !',2,2,60,'waste','2018-10-27 18:37:42.434928');
-insert into recommendation VALUES (6, 4, 'Dentifrice solide','Acheter du dentifrice solide c''est cool !',6,2,45,'waste','2018-10-27 18:37:42.434928');
+insert into recommendation VALUES (4, 1, 'Eponge durable','Acheter une éponge durable c''est cool !',2,1,40,'waste','2018-10-27 18:37:42.434928');
+insert into recommendation VALUES (5, 1, 'Brosse a dent durable','Acheter une brosse a dent durable c''est cool !',2,2,60,'waste','2018-10-27 18:37:42.434928');
+insert into recommendation VALUES (6, 1, 'Dentifrice solide','Acheter du dentifrice solide c''est cool !',6,2,45,'waste','2018-10-27 18:37:42.434928');
 insert into recommendation VALUES (7, 1, 'Je pense à éteindre mon frigo pendant mes vacances','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum feugiat eleifend. Vivamus non condimentum mauris. Pellentesque venenatis mauris quis diam dapibus, at dignissim turpis commodo. Pellentesque lacinia, lorem vitae faucibus efficitur, justo nulla ornare purus, ac ultricies magna nulla vel dolor. Sed tempor finibus sapien. Donec rutrum odio mi, in tempus est sollicitudin eget. Duis mollis arcu dui, sit amet feugiat orci laoreet non. In bibendum diam nec dui aliquam maximus. Nullam ac justo sem. Curabitur at ante porta, efficitur leo in, lacinia dui. In nec lectus in mauris vulputate gravida. Suspendisse quis feugiat leo, et luctus nibh. Praesent diam felis, dignissim nec ante vitae, laoreet accumsan diam.',6,2,45,'carbon','2018-12-12 18:37:42.434928');
 
 
--- green-client/src/components/items/PropositionItem.js
+++ green-client/src/components/items/PropositionItem.js
@@ -3,6 +3,8 @@ import {requestData} from "../../reducers/data";
 import React, {Component} from 'react'
 import {connect} from "react-redux";
 import {THUMBS_URL} from "../../utils/config";
+import calcul from "../../assets/calcul.png"
+import detail_bottom from "../../assets/details_bottom.png"
 
 
 class PropositionItem extends Component {
@@ -17,8 +19,12 @@ class PropositionItem extends Component {
             reco_difficulty_level: null}
     }
 
-    onSubmitedClick = () => {
+    onSuccessClick = () => {
         this.props.dispatch(requestData('GET',`/activity/${this.state.reco_id}`))
+    }
+
+    onFailClick = () => {
+    //    TODO:
     }
 
     componentDidMount () {
@@ -51,20 +57,20 @@ class PropositionItem extends Component {
             switch (this.state.reco_type) {
                 case 'carbon':
                     reco_category = "Sur la route"
-                    reco_img = THUMBS_URL + "car_color"
+                    reco_img = THUMBS_URL + "car_filled_color"
                     break;
                 case 'water':
                     reco_category = "Dans mon assiette"
-                    reco_img = THUMBS_URL + "food_color"
+                    reco_img = THUMBS_URL + "food_filled_color"
                     break;
                 case 'waste':
                     reco_category = "Chez moi"
-                    reco_img = THUMBS_URL + "home_color"
+                    reco_img = THUMBS_URL + "home_filled_color"
                     break;
             }
         }
 
-        let class_name = "carousel-item reco-card"
+        let class_name = "carousel-item"
         if (this.state && this.state.reco_first){
             class_name += " active"
         }
@@ -73,29 +79,101 @@ class PropositionItem extends Component {
             <div className={ class_name }>
                 <div className="carousel-item-inside-div">
                     <div className="carousel-center">
-                        <img className="card-img" src={ reco_img } alt="Card image cap"/>
-                        <div>
-                            <strong className="footprint-result-value">
-                                {this.state.reco_benefit}
-                            </strong> kg/CO2/an
+                        <div className="text-center">
+                            <img className="proposition-img" src={ reco_img } alt="Card image cap"/>
+                            <h6 className="proposition-title">
+                                { this.state.reco_title }
+                            </h6>
                         </div>
-                        <div className="reco-title">
-                            { this.state.reco_title }
+                        <div className="reco-benefit">
+                            <div className="text-center">
+                                <a className="reco-benefit-value"
+                                   data-toggle="collapse"
+                                   href={"#calcul"+ this.state.reco_id }
+                                   aria-expanded="false"
+                                   aria-controls={"calcul"+ this.state.reco_id }>
+                                    { this.state.reco_benefit } kgCO2/an
+                                    <img className="reco-benefit-calcul" src={ calcul } alt="calculation details"/>
+                                </a>
+                            </div>
+                            <div className="collapse"
+                                 id={"calcul"+ this.state.reco_id }>
+                                <div className="card card-body">
+                                    {/*{ this.state.reco_calcul }*/}
+                                    1g de plastique ~= 0,045 kgCO2<br />
+                                    1 tube de dentifrice = 50g de plastique<br />
+                                    25 tubes de dentifrice par an<br />
+                                    Impact total : 45 x 50 x 25 ~= 56 kgCO2<br />
+                                </div>
+                            </div>
                         </div>
-                        <div className="reco-content">
-                            { this.state.reco_content }
-                        </div>
-                        <div className="text-muted">
+                        <div className="proposition-difficulty text-center text-muted">
                             Difficulté: { this.state.reco_difficulty_level }
                         </div>
+
+                        <div className={"proposition-didyouknow prop-" + this.state.reco_type}>
+                            <h6>
+                                Le saviez-vous ?
+                            </h6>
+                            <div className="text-left">
+                                En moyenne un appareil électrique en veille utilise 20% de sa consommation
+                                    en état de marche.
+                            </div>
+                        </div>
+
+                        <div className="text-center">
+                            <a className="proposition-detail"
+                               data-toggle="collapse"
+                               href={"#detail"+ this.state.reco_id }
+                               aria-expanded="false"
+                               aria-controls={"detail"+ this.state.reco_id }>
+                                <h6>
+                                    En savoir plus
+                                </h6>
+                                <img className="details-img" src={ detail_bottom } alt="display details"/>
+                            </a>
+                        </div>
+
+                        <div className="collapse"
+                             id={"detail"+ this.state.reco_id }>
+                            <div className="reco-why">
+                                <h6>
+                                    <strong>Pourquoi le faire ?</strong>
+                                </h6>
+                                { this.state.reco_content }
+                            </div>
+                            <div className="reco-howto">
+                                <h6>
+                                    <strong>Comment le faire ?</strong>
+                                </h6>
+                                {/*{ this.state.reco_howto }*/}
+                                Prenez un truc et faite un machin.
+                                Le machin peut-être plus ou moins truc.
+                                Sinon vous povuez aussi faire des choses comme ça :
+                                <ul>
+                                    <li>avec un truc</li>
+                                    <li>avec une chose</li>
+                                    <li>avec un bidule aussi</li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <div className="proposition-actions">
+                            <div className="activity-actions-section proposition-add text-center col"
+                                 onClick={e => { e.preventDefault(); this.onSuccessClick();} }>
+                                C'est parti !
+                            </div>
+                            <div className="activity-actions-section proposition-drop text-center col"
+                                 onClick={e => { e.preventDefault(); this.onFailClick();} }>
+                                Pas pour moi
+                            </div>
+                        </div>
+
                     </div>
-                    <div className="carousel-caption">
-                        <button
-                            className="btn btn-secondary"
-                            onClick={e => { e.preventDefault(); this.onSubmitedClick();} }>
-                            C'est parti !
-                        </button>
-                    </div>
+                    {/*<div className="carousel-caption">*/}
+                        {/**/}
+                    {/*</div>*/}
                 </div>
             </div>
         )
